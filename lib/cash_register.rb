2566447e826961b2ca5d261004e25a9fa64548dc
Jeremy Bourne <jbourne25@yahoo.com>
2020-02-05 19:52:05 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -20,13 +20,8 @@ class CashRegister
   end
   
   def add_item(title, price, quantity = 1)
-    
-    
-   new_amount = price * quantity  
    
-   new_total = @total + new_amount
-   
-   @total = new_total
+   @total = @total + price * quantity
     
   end 
   
